d5acb58ad80af2c618cb12ea3c8bcb5efa93541a
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-09-24 01:29:19 +0200

    Create some handy macros in engine header

--- src/engine/libCore/Engine.h
+++ src/engine/libCore/Engine.h
@@ -2,6 +2,11 @@
 #include "Core.h"
 #include "Logger.h"
 
+#define KD_ENGINE kd::Engine::get()
+#define KD_LOG(MESSAGE) KD_ENGINE.logger.log(MESSAGE)
+#define KD_LOG_WARNING(MESSAGE) KD_ENGINE.logger.logWarning(MESSAGE)
+#define KD_LOG_ERROR(MESSAGE) KD_ENGINE.logger.logError(MESSAGE)
+
 KD_NAMESPACE_BEGIN
 
 class KD_CORE_DLL Engine
