351ca12b4595a0eab68bc76b262d7bb4ef992a15
Sebastian Kylander <sebastian@kylander.me>
2017-09-23 23:03:05 +0200

    Add Common header

---
+++ src/engine/libCore/Common.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// Platform macros
+#if defined(_WIN32)
+#define KD_DLL_EXPORT __declspec(dllexport)
+#define KD_DLL_IMPORT __declspec(dllimport)
+#define KD_ALIGN(N) __declspec(align(N))
+#else
+#define KD_DLL_EXPORT __attribute__((visibility("default")))
+#define KD_DLL_IMPORT __attribute__((visibility("default")))
+#define KD_ALIGN(N) __attribute__((__aligned__(N)))
+#endif
+
+// Namespace macros
+#define KD_NAMESPACE_BEGIN namespace kd {
+#define KD_NAMESPACE_END }
+
+// Misc macros
+#define KD_UNUSED(...) (void)(__VA_ARGS__)
