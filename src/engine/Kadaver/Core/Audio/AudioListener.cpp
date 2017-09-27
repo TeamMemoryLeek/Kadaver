893c651b45a9399631eaf7d804a62205e2b9bf6c
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-09-28 01:27:22 +0200

    Write audio listener

---
+++ src/engine/Kadaver/Core/Audio/AudioListener.cpp
@@ -0,0 +1,35 @@
+#include "AudioListener.h"
+
+KD_NAMESPACE_BEGIN
+
+AudioListener::AudioListener(const Vector3& position /*= Vector3::zero*/, 
+	const float& rotation /*= 0.f*/)
+	: position_(position)
+	, rotation_(rotation)
+{
+}
+
+void kd::AudioListener::playFromSource(SoundBuffer* buffer, Vector3 source)
+{
+	// Calculate distance
+	// TODO: Do this with squared length
+	float distance = (source - position_).length();
+	float volume = 1 / (distance * 0.02f + 1);
+
+	// Calculate panning
+	// TODO: Check if this is correct
+	Vector3 listenerToSource = (source - position_).normalized();
+	Vector3 direction = Vector3(sin(rotation_), 0.f, cos(rotation_));
+	direction.normalize();
+	source.y = 0.f;
+
+	Vector3 cross = direction.cross(listenerToSource);
+	float pan = cross.y;
+
+	buffer->setVolume(volume);
+	buffer->setPan(pan * 0.2f);
+	buffer->play();
+}
+
+KD_NAMESPACE_END
+
---
+++ src/engine/Kadaver/Core/Audio/AudioListener.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Kadaver/Core/Core.h"
+#include "Kadaver/Core/Math/Vector3.h"
+#include "SoundBuffer.h"
+
+KD_NAMESPACE_BEGIN
+
+// TODO: Dynamic positioning
+class KD_CORE_DLL AudioListener
+{
+public:
+	AudioListener(const Vector3& position = Vector3::zero, const float& rotation = 0.f);
+	void playFromSource(SoundBuffer* buffer, Vector3 source);
+
+	Vector3 position_;
+	float rotation_;
+};
+
+KD_NAMESPACE_END
