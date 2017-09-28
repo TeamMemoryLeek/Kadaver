e0d3c671f9d78c160ac1a42de3fc9f188edbb968
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-09-28 02:17:26 +0200

    Setup source and listener test case

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -2,6 +2,9 @@
 #include <Kadaver/Core/Window.h>
 #include <Kadaver/Core/Audio/AudioSystem.h>
 #include <Kadaver/Core/Audio/SoundBuffer.h>
+#include <Kadaver/Core/Audio/AudioListener.h>
+#include <Kadaver/Core/Audio/AudioSource.h>
+#include <Kadaver/Core/Math/Vector3.h>
 
 #include <iostream>
 
@@ -18,10 +21,15 @@ int main(int argc, char** argv)
 		kd::AudioSystem audioSystem(&window);
 		kd::SoundBuffer startUpAudio(&audioSystem);
 		startUpAudio.loadFromWave("data/audio/startup_sound.wav");
-		startUpAudio.play();
+		kd::AudioSource source(kd::Vector3(-1.0f, 0.0f, 0.0f));
+		kd::AudioListener listener(kd::Vector3::zero, 0.f);
+
+		source.playBuffer(&startUpAudio);
 
 		while (kd::Window::pollEvents())
 		{
+			source.update();
+			listener.rotation_ = startUpAudio.getProgress() * 360.0f;
 		}
 	}
 	catch (const std::exception& err)
